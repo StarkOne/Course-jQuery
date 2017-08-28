42fc51f58f5fcb9616505cc302f11bc420a0729a
StarkOne <Stark.pro40@gmail.com>
2017-08-28 14:22:19 +0700

    Creating a smooth scroll

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,17 @@
 $(function() {
 
-    $("#js-get-started").text('test');
+    $("#js-get-started").on('click', function(e) {
+
+    	e.preventDefault();
+
+    	var plansOffset = $("#js-plans").offset().top; // растояние с верха браузера до этого элемента
+    	
+    	// скрол до элемента
+    	$('html,body').animate({
+    		scrollTop : plansOffset
+    	}, 500);
+
+    });
 
     
 });
