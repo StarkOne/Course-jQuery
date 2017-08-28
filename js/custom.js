3124d656cebe4f2ee5c8e1d19f1add2db130c2f7
StarkOne <Stark.pro40@gmail.com>
2017-08-28 14:57:59 +0700

    fix the menu with the scroll

--- js/custom.js
+++ js/custom.js
@@ -1,17 +1,29 @@
 $(function() {
 
+		// плавные скрол
     $("#js-get-started").on('click', function(e) {
-
     	e.preventDefault();
 
     	var plansOffset = $("#js-plans").offset().top; // растояние с верха браузера до этого элемента
-    	
     	// скрол до элемента
     	$('html,body').animate({
     		scrollTop : plansOffset
     	}, 500);
-
     });
 
+    // фиксация меню 
+
+ 		var headerH = $('#js-header').height(),
+ 				navH = $('#js-nav-container').innerHeight();
+ 		$(document).on('scroll', function () {
+ 			var documentScroll = $(this).scrollTop();
+ 			if(documentScroll > headerH) {
+ 				$('#js-nav-container').addClass('fixed');
+ 				$('#js-header').css('marginTop', navH);
+ 			} else {
+ 				$('#js-nav-container').removeClass('fixed');
+ 				$('#js-header').removeAttr('style');
+ 			}
+ 		})
     
 });
